0db30eec6513e8df113353b6b796d8a5bb1c2d61
Md-Alamin2 <mdalaminn8733@gmail.com.com>
2022-08-14 00:18:10 +0600

    add sign-up component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
+import SignUp from "./components/auth/sign-up/SignUp";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1 className="text-3xl font-bold">React is working.</h1>
+        <h1 className="text-3xl font-bold">Note brew working.</h1>
+        <SignUp/>
       </div>
     </StrictMode>
   );
